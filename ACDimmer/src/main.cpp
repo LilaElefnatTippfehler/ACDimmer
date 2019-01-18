f64eca1d662f1085efd52848ad3539e54e2910c5
LilaElefnatTippfehler <andreas.wildmann@gmail.com>
2019-01-18 15:36:30 +0100

    Setting up WiFi and MQTT connection

--- ACDimmer/src/main.cpp
+++ ACDimmer/src/main.cpp
@@ -1,9 +1,68 @@
-#include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266mDNS.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266HTTPUpdateServer.h>
+#include <ESP8266HTTPClient.h>
+#include <PubSubClient.h>
+#include "config.h"
+
+#define DEVICE_NAME "Nachttisch"
+#define LED D1
+
+void callback(char* topic, byte* payload, unsigned int length);
+void reconnect();
+
+WiFiClient espClient;
+PubSubClient client(MQTT_IP,MQTT_PORT,callback,espClient);
+
 
 void setup() {
-  // put your setup code here, to run once:
+        Serial.begin(115200);
+        WiFi.mode(WIFI_STA);
+        WiFi.begin(WIFI_SSID,WIFI_PASS);
+        while (WiFi.status() != WL_CONNECTED) {
+                delay(500);
+                Serial.print(".");
+        }
+        while(!Serial);
+
+        Serial.print("Connected, IP address: ");
+        Serial.println(WiFi.localIP());
+
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+        if (!client.connected()) {
+                reconnect();
+        }
+        client.loop();
+
+}
+
+void reconnect() {
+        // Loop until we're reconnected
+        while (!client.connected()) {
+                Serial.print("Attempting MQTT connection...");
+                // Create a random client ID
+                String clientId = "ESP8266Client-";
+                clientId += DEVICE_NAME;
+                // Attempt to connect
+                if (client.connect(clientId.c_str(),MQTT_USR,MQTT_PW)) {
+                        Serial.print("connected as ");
+                        Serial.print(clientId.c_str()), Serial.println("");
+                        client.subscribe("/lampen/ada");
+                        client.subscribe("/lampen/nachttisch");
+
+                } else {
+                        Serial.print("failed, rc=");
+                        Serial.print(client.state());
+                        Serial.println(" try again in 5 seconds");
+                        // Wait 5 seconds before retrying
+                        delay(5000);
+                }
+        }
+}
+
+void callback(char* topic, byte* payload, unsigned int length){
+
 }

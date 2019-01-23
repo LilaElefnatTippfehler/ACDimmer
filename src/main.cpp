2a44b1690981b7ff5ce8a36ea0acf824a783bc8d
LilaElefnatTippfehler <andreas.wildmann@gmail.com>
2019-01-23 09:29:06 +0100

    Put touch gestures into own file

--- src/main.cpp
+++ src/main.cpp
@@ -7,29 +7,23 @@
 #include <Arduino.h>
 #include "ACDimmer.h"
 #include "config.h"
+#include "touchAutomat.h"
 
 
 #define TOUCH D7
-#define TOUCHTIME 1000
 
 void callback(char* topic, byte* payload, unsigned int length);
 void reconnect();
-void touchISR();
-void touchDTISR_up();
-void touchDTISR_down();
-void touchAutomat();
 void MQTTKeepTrack();
 void MQTTpubISR();
 
 
-uint8_t flag_touchR = 0;
-uint8_t flag_touchF = 0;
+
 uint8_t flag_MQTTpub = 0;
 int old_status = 0;
 int old_duty = 0;
 
 
-Ticker touchDimm;
 Ticker MQTTpub;
 WiFiClient espClient;
 PubSubClient client(MQTT_IP,MQTT_PORT,callback,espClient);
@@ -97,91 +91,7 @@ void reconnect() {
         }
 }
 
-void touchAutomat(){
-        static unsigned long touchT = 0;
-        static unsigned long betweenT = 0;
-        static int status = 0;
 
-        switch (status) {
-        case 0:
-                if(flag_touchR) {
-                        touchT = millis();
-                        status = 1;
-                }
-                break;
-        case 1:
-                if((touchT+TOUCHTIME)<=millis()) {
-                        status = 2;
-
-                }
-
-                if(flag_touchF) {
-                        if((touchT+TOUCHTIME)>=millis()) {
-                                status = 4;
-                                betweenT = millis();
-                                flag_touchR = 0;
-                                flag_touchF = 0;
-
-                        }
-                        break;
-                }
-                break;
-        case 2:
-                touchDimm.attach_ms(50, touchDTISR_up);
-                status = 3;
-                touchT = 0;
-                break;
-        case 3:
-                if(flag_touchF) {
-                        if((touchT + 1000)>= millis() && touchT) {
-                                touchDimm.detach();
-                                flag_touchR = 0;
-                                flag_touchF = 0;
-                                status = 0;
-                                //Serial.println("Touch losgelassen");
-                        }
-                        touchT = millis();
-                        flag_touchF = 0;
-                }
-                break;
-        case 4:
-                if((betweenT + 500)<=millis()) {
-                        if(dimmer_status()) {
-                                dimmer_off();
-                        }else{
-                                dimmer_on();
-                        }
-                        status = 0;
-                        break;
-                }
-                if(flag_touchR) {
-                        status = 5;
-                }
-
-
-                break;
-        case 5:
-                touchDimm.attach_ms(50, touchDTISR_down);
-                status = 6;
-                touchT = 0;
-                break;
-        case 6:
-                if(flag_touchF) {
-                        if((touchT + 1000)>= millis() && touchT) {
-                                touchDimm.detach();
-                                flag_touchR = 0;
-                                flag_touchF = 0;
-                                status = 0;
-                                //Serial.println("Touch losgelassen");
-                        }
-                        touchT = millis();
-                        flag_touchF = 0;
-                }
-                break;
-        }
-
-
-}
 
 void MQTTKeepTrack(){
         if(client.connected()) {
@@ -277,22 +187,4 @@ void callback(char* topic, byte* payload, unsigned int length){
 
 void MQTTpubISR(){
         flag_MQTTpub = 1;
-}
-
-void touchISR(){
-        if(flag_touchR) {
-                flag_touchF = 1;
-                flag_touchR = 0;
-        }else{
-                flag_touchR = 1;
-                flag_touchF = 0;
-        }
-
-}
-
-void touchDTISR_down(){
-        dimmer_down();
-}
-void touchDTISR_up(){
-        dimmer_up();
 }
---
+++ src/touchAutomat.cpp
@@ -0,0 +1,110 @@
+#include "touchAutomat.h"
+
+uint8_t flag_touchR = 0;
+uint8_t flag_touchF = 0;
+
+Ticker touchDimm;
+
+void touchAutomat(){
+        static unsigned long touchT = 0;
+        static unsigned long betweenT = 0;
+        static int status = 0;
+
+        switch (status) {
+        case 0:
+                if(flag_touchR) {
+                        touchT = millis();
+                        status = 1;
+                }
+                break;
+        case 1:
+                if((touchT+TOUCHTIME)<=millis()) {
+                        status = 2;
+
+                }
+
+                if(flag_touchF) {
+                        if((touchT+TOUCHTIME)>=millis()) {
+                                status = 4;
+                                betweenT = millis();
+                                flag_touchR = 0;
+                                flag_touchF = 0;
+
+                        }
+                        break;
+                }
+                break;
+        case 2:
+                touchDimm.attach_ms(50, touchDTISR_up);
+                status = 3;
+                touchT = 0;
+                break;
+        case 3:
+                if(flag_touchF) {
+                        if((touchT + 1000)>= millis() && touchT) {
+                                touchDimm.detach();
+                                flag_touchR = 0;
+                                flag_touchF = 0;
+                                status = 0;
+                                //Serial.println("Touch losgelassen");
+                        }
+                        touchT = millis();
+                        flag_touchF = 0;
+                }
+                break;
+        case 4:
+                if((betweenT + 500)<=millis()) {
+                        if(dimmer_status()) {
+                                dimmer_off();
+                        }else{
+                                dimmer_on();
+                        }
+                        status = 0;
+                        break;
+                }
+                if(flag_touchR) {
+                        status = 5;
+                }
+
+
+                break;
+        case 5:
+                touchDimm.attach_ms(50, touchDTISR_down);
+                status = 6;
+                touchT = 0;
+                break;
+        case 6:
+                if(flag_touchF) {
+                        if((touchT + 1000)>= millis() && touchT) {
+                                touchDimm.detach();
+                                flag_touchR = 0;
+                                flag_touchF = 0;
+                                status = 0;
+                                //Serial.println("Touch losgelassen");
+                        }
+                        touchT = millis();
+                        flag_touchF = 0;
+                }
+                break;
+        }
+
+
+}
+
+void touchISR(){
+        if(flag_touchR) {
+                flag_touchF = 1;
+                flag_touchR = 0;
+        }else{
+                flag_touchR = 1;
+                flag_touchF = 0;
+        }
+
+}
+
+void touchDTISR_down(){
+        dimmer_down();
+}
+void touchDTISR_up(){
+        dimmer_up();
+}
